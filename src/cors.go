513f3965b032262e1228d741f85e6a4e1f1a04d5
lbarto12 <74741908+lbarto12@users.noreply.github.com>
2025-06-19 16:31:46 -0400

    Added documentation

--- src/cors.go
+++ src/cors.go
@@ -6,7 +6,16 @@ import (
 	"github.com/rs/cors"
 )
 
-func ConfigureCors() *cors.Options { // Required, if you do not want cors, return nil from this method
+/*
+Required
+
+This method allows you to configure cors from here.
+The *cors.Options object that is returned is used as
+the settings when the server is created.
+
+If you do not want cors in your application, then return nil from this function.
+*/
+func ConfigureCors() *cors.Options {
 	return &cors.Options{
 		AllowedOrigins:   []string{"*"},
 		AllowedMethods:   []string{http.MethodGet, http.MethodPost},
--- src/main.go
+++ src/main.go
@@ -6,10 +6,18 @@ import (
 )
 
 /*
-	Put your code in this file, this represents your API
-*/
+Required
 
-func Main(server *api.Server) { // Required
+This is your application's entry point, use this as your main function
+when initializing your app.
+
+The server will be automatically launched, there is no need to
+launch it from here.
+
+Params:
+`server`: The server obect, this allows you to modify properties of the server before it is launched
+*/
+func Main(server *api.Server) {
 	fmt.Println("In main")
 
 }
